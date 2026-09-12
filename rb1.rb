a418df52841622e0dcbe1aa7330f4761314407af
Hc <rna376388@gmail.com>
2026-09-13 02:54:14 +0800

    update rb1.rb

--- rb1.rb
+++ rb1.rb
@@ -1,3 +1,4 @@
+print "Halo"
 nama = "Heart candy"
 umur = "17"
 agama = "Islam"
@@ -5,9 +6,18 @@ agama = "Islam"
 puts "Halo sayang perkenalkan nama saya " + nama
 puts "dan umur saya " + umur
 puts "Agama saya " + agama
+
 print "Apakah kamu ingin berteman dengan ku? (ya/tidak) :"
-
 jawaban = gets.chomp
-
 puts "Terkasih sudah menjawab pertanyaan itu #{jawaban}"
 
+if jawaban == 'ya'
+  puts "Senang sekali mendengarnya!"
+elsif jawaban == 'tidak'
+  puts "oh kenapa?"
+  puts "Apakah Kamu tidak menyukai aku? :"
+  alasan = gets.chomp
+  puts "Baiklah, aku mengerti. Kamu sedang sibuk '#{alasan}'"
+else
+  puts "Senang sekali bertemu dengan mu!, Semoga kita bisa bertemu lagi."
+end
