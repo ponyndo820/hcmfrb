b82da4aa21d6ce44b0550ced6c9085520ca6b569
Hc <rna376388@gmail.com>
2026-09-13 02:09:42 +0800

    update rb1.rb

--- rb1.rb
+++ rb1.rb
@@ -1,11 +1,9 @@
-# Mendefinisikan variabel dengan berbagai tipe data
-nama_pemilik = "Ponyndo"
-bahasa = "Ruby"
-tingkat_keseruan = 10
-apakah_menyenangkan = true
+nama = "Heart candy"
+umur = "17"
+agama = "Islam"
+true = ya
 
-# Menampilkan isi variabel ke layar
-puts "Halo, nama saya " + nama_pemilik
-puts "Saya sedang belajar bahasa " + bahasa
-puts "Skor keseruan: #{tingkat_keseruan}"
-puts "Apakah menyenangkan? #{apakah_menyenangkan}"
+puts "Halo sayang perkenalkan nama saya" + nama
+puts = "dan umur saya" + umur
+puts = "Agama saya" + agama
+puts = "Apakah kamu ingin menjadi teman ku" + true
