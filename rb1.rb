99cc06d35b3cda6a7152fd101555e4ab1b0b1ec7
Hc <rna376388@gmail.com>
2026-09-13 02:27:23 +0800

    update rb1.rb

--- rb1.rb
+++ rb1.rb
@@ -2,6 +2,11 @@ nama = "Heart candy"
 umur = "17"
 agama = "Islam"
 
+ya = gets.chomp
+tidak = gets.chomp
+
 puts "Halo sayang perkenalkan nama saya " + nama
 puts "dan umur saya " + umur
 puts "Agama saya " + agama
+puts "Apakah kamu ingin berteman dengan ku ? #{ya}"
+
