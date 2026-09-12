504ed147c0d03c9cd22c6372b556ad15f838a857
Hc <rna376388@gmail.com>
2026-09-13 02:14:31 +0800

    update rb1.rb

--- rb1.rb
+++ rb1.rb
@@ -2,6 +2,6 @@ nama = "Heart candy"
 umur = "17"
 agama = "Islam"
 
-puts "Halo sayang perkenalkan nama saya" + nama
-puts = "dan umur saya" + umur
-puts = "Agama saya" + agama
+puts "Halo sayang perkenalkan nama saya " + nama
+puts = "dan umur saya " + umur
+puts = "Agama saya " + agama
