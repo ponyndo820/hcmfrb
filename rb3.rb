37aee009c036ef74c235a6ecd2c10797832edb13
Hc <rna376388@gmail.com>
2026-09-14 01:57:17 +0800

    update rb3.rb

--- rb3.rb
+++ rb3.rb
@@ -1,12 +1,14 @@
 require 'open-uri'
-require 'cgi'
 
 def ambil_screenshot(url, mode = "desktop")
-  resolusi = mode == "mobile" ? "&viewport.width=375&viewport.height=812" : "&viewport.width=1280&viewport.height=800"
-  
   nama_file = "web_screenshot.png"
-  url_encoded = CGI.escape(url)
-  api_url = "https://api.microlink.io/?url=#{url_encoded}&screenshot=true&embed=screenshot.url#{resolusi}"
+  
+  # Menggunakan API dari Thum.io yang lebih handal untuk YouTube
+  if mode == "mobile"
+    api_url = "https://image.thum.io/get/iphone/#{url}"
+  else
+    api_url = "https://image.thum.io/get/width/1280/crop/800/#{url}"
+  end
 
   puts "\n[+] Memproses render screenshot dari: #{url}..."
 
@@ -28,17 +30,15 @@ end
 
 puts "=== GENERATOR SCREENSHOT WEB ==="
 print "Masukkan URL Website: "
-# HAPUS .downcase di sini agar karakter huruf besar pada URL/ID YouTube tetap terjaga!
 input_url = gets.chomp.strip 
 
-# Pengecekan skema HTTP/HTTPS tanpa mengganggu huruf kapital di bagian ID
+# Memastikan URL memiliki https:// tanpa mengubah huruf kapital
 unless input_url.match?(%r{\Ahttps?://}i)
   input_url = "https://" + input_url
 end
 
 print "Pilih Tampilan (1: Desktop, 2: Mobile) [1]: "
 pilihan = gets.chomp.strip
-
 mode_tampilan = (pilihan == "2") ? "mobile" : "desktop"
 
 ambil_screenshot(input_url, mode_tampilan)
